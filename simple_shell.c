c34558c2a2797d308d15f5dccf9be22138635cb6
anfepema700 <anfepema700>
2021-12-07 08:31:17 -0800

    simple_shell contains main of project

---
+++ simple_shell.c
@@ -0,0 +1,52 @@
+#include "shell.h"
+
+/**
+ *main - simple_shell
+ *Return 0 is success
+**/
+
+int main(void)
+{
+	ssize_t len= 0; 
+	char *buff = NULL, *value, *pathname, **arv;
+	size_t size = 0;
+	list_path *head = '\0';
+	void (*f)(char **);
+
+	signal(SIGINT, _control_c);
+
+	while(len != _end_of_file)
+	{
+		_terminal();
+		len = getline(&buff, &size, stdin);
+		_end_of_file(len, buff);
+		arv = splitstring(buff, "\n");
+		if (!arv || !arv[0])
+			execute(arv);
+		else
+		{
+			value = _getenv("PATH");
+			head = linkpath(value);
+			pathname = _which(arv[0], head);
+			f = checkbuild(arv);
+
+			if(f)
+			{
+				free(buff);
+				f(arv);
+			}
+			else if(!pathname)
+				execute(arv);
+			else if(pathname)
+			{
+				free(arv[0]);
+				arv[0] = pathname;
+				execute(arv);
+			}
+		}
+	}
+	free_list(head);
+	freearv(arv);
+	free(buff);
+	return(0);
+}
