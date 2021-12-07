c338df75c96b9c00167d78a5ac46cbaa1517fd7b
hedaecra <3883@holbertonschool.com>
2021-12-07 11:10:07 -0800

    update file linexe

--- line_exe.c
+++ line_exe.c
@@ -109,3 +109,28 @@ free(arv[i]);
 
 free(arv);
 }
+
+/**
+ * execute - executes a command
+ * @argv: array of arguments
+ */
+
+void execute(char **argv)
+{
+int d, status;
+
+if (!argv || !argv[0])
+return;
+d = fork();
+if (d == -1)
+{
+perror(_getenv("_"));
+}
+if (d == 0)
+{
+execve(argv[0], argv, environ);
+perror(argv[0]);
+exit(EXIT_FAILURE);
+}
+wait(&status);
+}
