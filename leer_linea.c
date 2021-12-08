28b5f5dc78ccd9d5ddf607aac4ccb122aa4bb990
hedaecra <3883@holbertonschool.com>
2021-12-08 10:27:13 -0800

    update file betty

--- leer_linea.c
+++ leer_linea.c
@@ -10,12 +10,11 @@
 char *read_input(char **input, size_t *bufferinput)
 {
 	ssize_t readchar = 0;
+
 	readchar = getline(input, bufferinput, stdin);
 
-	/* check if getline works */
 	if (readchar == EOF)
 	{
-		/* reach the end */
 		free(*input);
 		exit(EXIT_SUCCESS);
 	}
