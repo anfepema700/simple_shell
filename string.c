40e11ec0e177e13783e3507b82529ab6d38edb58
anfepema700 <anfepema700>
2021-12-08 07:40:52 -0800

    corrected string.c compiler and checks results

--- string.c
+++ string.c
@@ -1,116 +1,114 @@
 #include "shell.h"
 
 /**
-*_strdup - duplicate the newly alocated space in memory
-*contains a copy of the string given as a parameter
-*use for protect the type initial.
-*@str: pointer to a string
-*Return: pointer to a string
-**/
-
+ *  * _strdup - returns a pointer to a newly allocated space in memory, which
+ *   * contains a copy of the string given as a parameter
+ *    * @str: pointer to a string
+ *     * Return: pointer to a string
+ *      */
 char *_strdup(char *str)
 {
-	int i, l;
-	char *new;
+		int i, l;
+			char *new;
 
-	if (!str)
-	{
-		return (NULL);
-	}
-	for (l = 0; str[l] != '\0';)
-	{
-		l++;
-	}
-		new = malloc(sizeof(char) * l + 1);
-	if (!new)
-	{
-		return (NULL);
-	}
-	for (i = 0; i < l; i++)
-	{
-		new[i] = str[i];
-	}
-		new[l] = str[i];
-	return (new);
+				if (!str)
+						{
+									return (NULL);
+										}
+					for (l = 0; str[l] != '\0';)
+							{
+										l++;
+											}
+						new = malloc(sizeof(char) * l + 1);
+							if (!new)
+									{
+												return (NULL);
+													}
+								for (i = 0; i < l; i++)
+										{
+													new[i] = str[i];
+														}
+									new[l] = str[l];
+										return (new);
 }
 
 /**
-*concat_all - concats the three string in allocated memory
-*@name: first string
-*@sep: second string
-*@value: third string
-*Return: pointer tothe new string
-**/
-
+ *  * concat_all - concats 3 strings in a newly allocated memory
+ *   * @name: first string
+ *    * @sep: second string
+ *     * @value: Third string
+ *      * Return: pointer to the new string
+ *       */
 char *concat_all(char *name, char *sep, char *value)
 {
-	char *result;
-	int l1, l2, l3, i, k;
+		char *result;
+			int l1, l2, l3, i, k;
 
-	l1 = _strlen(name);
-	l2 = _strlen(sep);
-	l3 = _strlen(value);
-	result = malloc(l1 + l2 + l3 + 1);
-	
-	if (!result)
-		return (NULL);
+				l1 = _strlen(name);
+					l2 = _strlen(sep);
+						l3 = _strlen(value);
 
-	for(i = 0; name[i]; i++)
-		result[i] = name[i];
-	k = i;
-	for (i = 0; sep[i]; i++)
-		result[k + i] = sep[i];
-	k = k + 1;
+							result = malloc(l1 + l2 + l3 + 1);
+								if (!result)
+											return (NULL);
 
-	for (i = 0; value[i]; i++)
-		result[k + i] = value[i];
-	k = k + i;
+									for (i = 0; name[i]; i++)
+												result[i] = name[i];
+										k = i;
 
-	result[k] = '\0';
+											for (i = 0; sep[i]; i++)
+														result[k + i] = sep[i];
+												k = k + i;
 
-	return (result);
+													for (i = 0; value[i]; i++)
+																result[k + i] = value[i];
+														k = k + i;
+
+															result[k] = '\0';
+
+																return (result);
 }
 
 /**
- *_strlen - gives the length of a string received.
- *@s: pointer to the string
- *Return: the leng of string
-**/
-
+ *  * _strlen - it gives the length of a string
+ *   * @s: pointer to the string
+ *    * Return: the length of string
+ *     */
 int _strlen(char *s)
 {
-	int i = 0;
+		int i = 0;
 
-	while (*(s + 1) != '\0')
-	{
-		i++;
-	}
-	return (i);
+			while (*(s + i) != '\0')
+					{
+								i++;
+									}
+				return (i);
 }
 
 /**
- *_putchar - write the character
- *@c: character for print
- *Return: 1 success -1 error
-**/
-
+ *  * _putchar - writes the character c to stdout
+ *   * @c: The character to print
+ *    *
+ *     * Return: On success 1.
+ *      * On error, -1 is returned, and errno is set appropriately.
+ *       */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+		return (write(1, &c, 1));
 }
 
 /**
- *_puts - prints a string
- *@str: pointer to string
- **/
+ *  * _puts - prints a string
+ *   * @str: pointer to string
+ *    */
 
 void _puts(char *str)
 {
-	int i = 0;
+		int i = 0;
 
-	while (str[i])
-	{
-		_putchar(str[i]);
-		i++;
-	}
+			while (str[i])
+					{
+								_putchar(str[i]);
+										i++;
+											}
 }
